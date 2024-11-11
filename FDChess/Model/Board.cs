1c2d8c0fd7242c7d285b145e53b17633f208c8f1
Filip Dadgar <filip.dadgar.ext@t-hive.io>
2024-11-11 13:00:06 +0100

    minor debug fix

--- FDChess/Model/Board.cs
+++ FDChess/Model/Board.cs
@@ -116,12 +116,12 @@ namespace FDChess.Model
         
         public bool IsPositionUnderAttack(Position position, string color)
         {
-            Console.WriteLine($"Checking if position {position} is under attack for {color}");
+            Console.WriteLine($"Checking if position {position.Column} {position.Row} is under attack for {color}");
             foreach (var piece in Pieces.Where(p => p.Color != color && !p.IsRemoved))
             {
                 if (piece.IsMoveValid(position, this))
                 {
-                    Console.WriteLine($"Position {position} is under attack by {piece.Color} {piece.Name} at {piece.Position}");
+                    Console.WriteLine($"Position {position.Column} {position.Row} is under attack by {piece.Color} {piece.Name} at {piece.Position}");
                     return true;
                 }
             }
